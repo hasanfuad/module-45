bf904b48b8fe5945dbde3de158e5f5accb52fa35
hasanfuad <hasanmdfuad@hotmail.com>
2021-08-31 20:59:42 +0600

    commit some more on node js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors());
+
+
 app.get('/', (req, res) => {
-    res.send("Thank you");
+    res.send("Hello");
+});
+
+const friendList = ["Moniyem", "Hasib", "Tariq", "Wasif"];
+
+app.get('/friend/:id', (req, res) => {
+    const user = req.params.id;
+    const name = friendList[user];
+    res.send({user, name});
 })
 
-app.listen(3000, () => {
-  console.log("Server is listening from 3000");
-});
+app.listen('3000', ()=>{
+    console.log("Server is listening from 3000");
+})
